a3c57c7ed11bc46a8d6e0e4d33f714c36928d644
C-YooJin <ioi_3312@naver.com>
2021-05-25 09:12:11 +0900

    Initial commit

---
+++ src/main/java/com/cfww/croiffle/CroiffleApplication.java
@@ -0,0 +1,13 @@
+package com.cfww.croiffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroiffleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CroiffleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfww/croiffle/CroiffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfww.croiffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroiffleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
